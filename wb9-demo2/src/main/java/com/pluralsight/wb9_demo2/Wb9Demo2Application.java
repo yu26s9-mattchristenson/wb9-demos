f4f2a37d810fac309bd65220c3d2cd9ac56b9337
mattchristenson <matt@rcc.team>
2026-06-12 12:35:14 -0500

    add demo 2

---
+++ wb9-demo2/src/main/java/com/pluralsight/wb9_demo2/Wb9Demo2Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.wb9_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wb9Demo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wb9Demo2Application.class, args);
+	}
+
+}
