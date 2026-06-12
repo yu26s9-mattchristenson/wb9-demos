63b6e61d3afba004922c22ad2cd08b69eb75eda4
mattchristenson <matt@rcc.team>
2026-06-12 12:27:00 -0500

    added spring starter files on demo 1

---
+++ wb9-demo1/src/main/kotlin/com/pluralsight/wb9_demo1/Wb9Demo1Application.kt
@@ -0,0 +1,11 @@
+package com.pluralsight.wb9_demo1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Wb9Demo1Application
+
+fun main(args: Array<String>) {
+	runApplication<Wb9Demo1Application>(*args)
+}
