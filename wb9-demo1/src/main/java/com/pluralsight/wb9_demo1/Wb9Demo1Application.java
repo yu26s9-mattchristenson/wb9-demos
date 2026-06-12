f002b4a1c7591247136092a43049555aeceed913
mattchristenson <matt@rcc.team>
2026-06-12 12:29:21 -0500

    fixed this, used kotlin instead of java, back to java now

---
+++ wb9-demo1/src/main/java/com/pluralsight/wb9_demo1/Wb9Demo1Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.wb9_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wb9Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wb9Demo1Application.class, args);
+	}
+
+}
---
+++ wb9-demo1/src/test/java/com/pluralsight/wb9_demo1/Wb9Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.wb9_demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wb9Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
